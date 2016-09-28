26a3137cdb728f8eaa230eed18b9e95710611e5b
Gary <gary.mayfield@hotmail.co.uk>
2016-09-28 14:01:25 +0100

    Overloads and templates

---
+++ Program structure/functionTemplate2/functionTemplate2/Source.cpp
@@ -0,0 +1,20 @@
+// function templates
+#include <iostream>
+using namespace std;
+
+template <class T, class U>
+bool are_equal(T a, U b)
+{
+	return (a == b);
+}
+
+int main()
+{
+	if (are_equal(10, 10.0))
+		cout << "x and y are equal\n";
+	else
+		cout << "x and y are not equal\n";
+
+	getchar();
+	return 0;
+}
---
+++ Program structure/templateArguments/templateArguments/Source.cpp
@@ -0,0 +1,18 @@
+// template arguments
+#include <iostream>
+using namespace std;
+
+template <class T, int N>
+T fixed_multiply(T val)
+{
+	return val * N;
+}
+
+int main()
+{
+	cout << fixed_multiply<int, 2>(10) << '\n';
+	cout << fixed_multiply<int, 3>(10) << '\n';
+
+	getchar();
+	return 0;
+}
