ccc7a110b6714277493a9eb835aa6f414307b41d
Gary <gary.mayfield@hotmail.co.uk>
2016-09-28 11:08:57 +0100

    Functions

---
+++ Program structure/voidFunction/voidFunction/Source.cpp
@@ -0,0 +1,16 @@
+// void function example
+#include <iostream>
+using namespace std;
+
+void printMessage()
+{
+	cout << "I'm a function!";
+}
+
+int main()
+{
+	printMessage();
+	
+	getchar();
+	return 0;
+}
